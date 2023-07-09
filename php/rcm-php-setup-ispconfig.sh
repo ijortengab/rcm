3bc3c0fa8fe92657ab1871ea61de518c320dd492
IjorTengab <m_roji28@yahoo.com>
2023-07-09 12:03:58 +0700

    Memindahkan common function tepat dibawah parse options.

--- php/rcm-php-setup-ispconfig.sh
+++ php/rcm-php-setup-ispconfig.sh
@@ -17,6 +17,25 @@ done
 set -- "${_new_arguments[@]}"
 unset _new_arguments
 
+# Common Functions.
+red() { echo -ne "\e[91m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
+green() { echo -ne "\e[92m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
+yellow() { echo -ne "\e[93m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
+blue() { echo -ne "\e[94m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
+magenta() { echo -ne "\e[95m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
+error() { echo -n "$INDENT" >&2; red "$@" >&2; echo >&2; }
+success() { echo -n "$INDENT" >&2; green "$@" >&2; echo >&2; }
+chapter() { echo -n "$INDENT" >&2; yellow "$@" >&2; echo >&2; }
+title() { echo -n "$INDENT" >&2; blue "$@" >&2; echo >&2; }
+code() { echo -n "$INDENT" >&2; magenta "$@" >&2; echo >&2; }
+x() { echo >&2; exit 1; }
+e() { echo -n "$INDENT" >&2; echo "$@" >&2; }
+_() { echo -n "$INDENT" >&2; echo -n "$@" >&2; }
+_,() { echo -n "$@" >&2; }
+_.() { echo >&2; }
+__() { echo -n "$INDENT" >&2; echo -n '    ' >&2; [ -n "$1" ] && echo "$@" >&2 || echo -n  >&2; }
+____() { echo >&2; [ -n "$delay" ] && sleep "$delay"; }
+
 # Functions.
 printVersion() {
     echo '0.2.0'
@@ -50,25 +69,6 @@ EOF
 # Help and Version.
 [ -n "$help" ] && { printHelp; exit 1; }
 [ -n "$version" ] && { printVersion; exit 1; }
-
-# Common Functions.
-red() { echo -ne "\e[91m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
-green() { echo -ne "\e[92m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
-yellow() { echo -ne "\e[93m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
-blue() { echo -ne "\e[94m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
-magenta() { echo -ne "\e[95m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
-error() { echo -n "$INDENT" >&2; red "$@" >&2; echo >&2; }
-success() { echo -n "$INDENT" >&2; green "$@" >&2; echo >&2; }
-chapter() { echo -n "$INDENT" >&2; yellow "$@" >&2; echo >&2; }
-title() { echo -n "$INDENT" >&2; blue "$@" >&2; echo >&2; }
-code() { echo -n "$INDENT" >&2; magenta "$@" >&2; echo >&2; }
-x() { echo >&2; exit 1; }
-e() { echo -n "$INDENT" >&2; echo "$@" >&2; }
-_() { echo -n "$INDENT" >&2; echo -n "$@" >&2; }
-_,() { echo -n "$@" >&2; }
-_.() { echo >&2; }
-__() { echo -n "$INDENT" >&2; echo -n '    ' >&2; [ -n "$1" ] && echo "$@" >&2 || echo -n  >&2; }
-____() { echo >&2; [ -n "$delay" ] && sleep "$delay"; }
 
 # Functions.
 downloadApplication() {
