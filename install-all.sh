e534d1764b93f2e0c40a0f141ac1c6e92240a6bc
IjorTengab <m_roji28@yahoo.com>
2023-07-13 06:05:06 +0700

    Menambah file install-all.sh.

---
+++ install-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if command -v git >/dev/null;then
+    while read line; do
+        if [[ $line =~ .sh$ ]];then
+            chmod a+x "$line"
+            echo -n cd /usr/local/bin'; '
+            echo ln -sf "$PWD/$line"
+            ln -sf "$PWD/$line" /usr/local/bin/$(basename "$line")
+        fi
+    done <<< `git ls-files`
+else
+    find * -type f -name '*.sh' | while read line; do
+        chmod a+x "$line"
+        echo -n cd /usr/local/bin'; '
+        echo ln -sf "$PWD/$line"
+        ln -sf "$PWD/$line" /usr/local/bin/$(basename "$line")
+    done
+fi
+
+cd /usr/local/bin
+ln -sf rcm.sh rcm
+cd - >/dev/null
