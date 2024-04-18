9df2eb907d7101f3ec5ccb38c0680ff86882d4df
ijortengab <m_roji28@yahoo.com>
2024-04-18 08:24:39 +0700

    Update certbot/rcm-certbot-obtain-certificates.sh, mendukung dns authenticator standalone.

--- certbot/rcm-certbot-obtain-certificates.sh
+++ certbot/rcm-certbot-obtain-certificates.sh
@@ -12,6 +12,8 @@ while [[ $# -gt 0 ]]; do
         --domain=*) domain="${1#*=}"; shift ;;
         --domain) if [[ ! $2 == "" && ! $2 =~ ^-[^-] ]]; then domain="$2"; shift; fi; shift ;;
         --fast) fast=1; shift ;;
+        --root-sure) root_sure=1; shift ;;
+        --standalone) dns_authenticator=standalone; shift ;;
         --subdomain=*) subdomain="${1#*=}"; shift ;;
         --subdomain) if [[ ! $2 == "" && ! $2 =~ ^-[^-] ]]; then subdomain="$2"; shift; fi; shift ;;
         --[^-]*) shift ;;
@@ -46,19 +48,19 @@ printVersion() {
 }
 printHelp() {
     title RCM Certbot Obtain
-    _ 'Variation '; yellow Certificates Wildcard; _.
+    _ 'Variation '; yellow Certificates Default; _.
     _ 'Version '; yellow `printVersion`; _.
     _.
     cat << 'EOF'
-Usage: rcm-certbot-obtain-certificates-wildcard.sh [options]
+Usage: rcm-certbot-obtain-certificates.sh [options]
 
 Options:
    --subdomain
         Set the subdomain if any.
-   --domain
+   --domain *
         Set the domain.
-   --dns-authenticator
-        Available value: digitalocean.
+   --dns-authenticator *
+        Available value: digitalocean, standalone.
 
 Global Options:
    --fast
@@ -196,7 +198,7 @@ link_symbolic() {
 }
 
 # Title.
-title rcm-certbot-obtain-certificates-wildcard.sh
+title rcm-certbot-obtain-certificates.sh
 ____
 
 # Require, validate, and populate value.
@@ -218,8 +220,13 @@ fi
 code 'fqdn_project="'$fqdn_project'"'
 case "$dns_authenticator" in
     digitalocean) ;;
+    standalone) ;;
     *) dns_authenticator=
 esac
+if [ -z "$dns_authenticator" ];then
+    error "Argument --dns-authenticator required."; x
+fi
+
 until [[ -n "$dns_authenticator" ]];do
     _ Available value:' '; yellow digitalocean.; _.
     _; read -p "Argument --dns-authenticator required: " dns_authenticator
@@ -229,10 +236,12 @@ until [[ -n "$dns_authenticator" ]];do
     esac
 done
 code 'dns_authenticator="'$dns_authenticator'"'
-TOKEN=${TOKEN:=$HOME/.$dns_authenticator-token.txt}
-code 'TOKEN="'$TOKEN'"'
-TOKEN_INI=${TOKEN_INI:=$HOME/.$dns_authenticator-token.ini}
-code 'TOKEN_INI="'$TOKEN_INI'"'
+if [[ "$dns_authenticator" == 'digitalocean' ]]; then
+    TOKEN=${TOKEN:=$HOME/.$dns_authenticator-token.txt}
+    code 'TOKEN="'$TOKEN'"'
+    TOKEN_INI=${TOKEN_INI:=$HOME/.$dns_authenticator-token.ini}
+    code 'TOKEN_INI="'$TOKEN_INI'"'
+fi
 ____
 
 if [ -z "$root_sure" ];then
@@ -245,68 +254,90 @@ if [ -z "$root_sure" ];then
     ____
 fi
 
-if [[ "$dns_authenticator" == 'digitalocean' ]]; then
-    chapter Mengecek DNS Authenticator
-    __ Menggunakan DNS Authenticator '`'digitalocean'`'
+chapter Prepare arguments.
+domain="$fqdn_project"
+code 'domain="'$fqdn_project'"'
+____
+
+chapter Mengecek certificates atas nama '`'$domain'`'
+notfound=
+if certbot certificates 2>/dev/null | grep -q -o "Certificate Name: ${domain}";then
+    __ Certificate obtained.
+else
+    __ Certificate not found.
+    notfound=1
+fi
+____
+
+if [ -n "$notfound" ];then
+    chapter Prepare arguments.
+    email=$(certbot show_account 2>/dev/null | grep -o -P 'Email contact: \K(.*)')
+    if [ -n "$email" ];then
+        __ Certbot account has found: "$email"
+    else
+        email="${MAILBOX_HOST}@${domain}"
+    fi
+    code 'email="'$email'"'
     ____
 
-    chapter Mengecek Token
-    fileMustExists "$TOKEN"
-    digitalocean_token=$(<$TOKEN)
-    __; magenta 'digitalocean_token="'$digitalocean_token'"'; _.
-    isFileExists "$TOKEN_INI"
-    if [ -n "$notfound" ];then
-        __ Membuat file "$TOKEN_INI"
-        cat << EOF > "$TOKEN_INI"
+    chapter Mengecek DNS Authenticator
+    __ Menggunakan DNS Authenticator '`'$dns_authenticator'`'
+    ____
+
+    if [[ "$dns_authenticator" == 'digitalocean' ]]; then
+        chapter Mengecek Token
+        fileMustExists "$TOKEN"
+        digitalocean_token=$(<$TOKEN)
+        __; magenta 'digitalocean_token="'$digitalocean_token'"'; _.
+        isFileExists "$TOKEN_INI"
+        if [ -n "$notfound" ];then
+            __ Membuat file "$TOKEN_INI"
+            cat << EOF > "$TOKEN_INI"
 dns_digitalocean_token = $digitalocean_token
 EOF
-    fi
-    fileMustExists "$TOKEN_INI"
-    if [[ $(stat "$TOKEN_INI" -c %a) == 600 ]];then
-        __ File  '`'"$TOKEN_INI"'`' memiliki permission '`'600'`'.
-    else
-        __ File  '`'"$TOKEN_INI"'`' tidak memiliki permission '`'600'`'.
-        tweak=1
-    fi
-    if [ -n "$tweak" ];then
-        chmod 600 "$TOKEN_INI"
-        if [[ $(stat --cached=never "$TOKEN_INI" -c %a) == 600 ]];then
-            __; green File  '`'"$TOKEN_INI"'`' memiliki permission '`'600'`'.; _.
-        else
-            __; red File  '`'"$TOKEN_INI"'`' tidak memiliki permission '`'600'`'.; x
         fi
-    fi
-    ____
-
-    chapter Prepare arguments.
-    domain="$fqdn_project"
-    code 'domain="'$fqdn_project'"'
-    ____
-
-    chapter Mengecek certificates atas nama '`'$domain'`'
-    notfound=
-    if certbot certificates 2>/dev/null | grep -q -o "Certificate Name: ${domain}";then
-        __ Certificate obtained.
-    else
-        __ Certificate not found.
-        notfound=1
-    fi
-    ____
-
-    if [ -n "$notfound" ];then
-        chapter Prepare arguments.
-        email=$(certbot show_account 2>/dev/null | grep -o -P 'Email contact: \K(.*)')
-        if [ -n "$email" ];then
-            __ Certbot account has found: "$email"
+        fileMustExists "$TOKEN_INI"
+        if [[ $(stat "$TOKEN_INI" -c %a) == 600 ]];then
+            __ File  '`'"$TOKEN_INI"'`' memiliki permission '`'600'`'.
         else
-            email="${MAILBOX_HOST}@${domain}"
+            __ File  '`'"$TOKEN_INI"'`' tidak memiliki permission '`'600'`'.
+            tweak=1
         fi
-        code 'email="'$email'"'
+        if [ -n "$tweak" ];then
+            chmod 600 "$TOKEN_INI"
+            if [[ $(stat --cached=never "$TOKEN_INI" -c %a) == 600 ]];then
+                __; green File  '`'"$TOKEN_INI"'`' memiliki permission '`'600'`'.; _.
+            else
+                __; red File  '`'"$TOKEN_INI"'`' tidak memiliki permission '`'600'`'.; x
+            fi
+        fi
         ____
 
         chapter Request Certificate.
-        code certbot certonly --non-interactive --agree-tos --email='"'$email'"' --domain='"'$domain'"' -d='"''*.'$domain'"' --dns-digitalocean --dns-digitalocean-credentials='"'$TOKEN_INI'"'
-        certbot certonly --non-interactive --agree-tos --email="$email" --domain="$domain" -d="*.$domain" --dns-digitalocean --dns-digitalocean-credentials="$TOKEN_INI"
+        code certbot certonly --non-interactive --agree-tos --email='"'$email'"' --domain='"'$domain'"' --dns-digitalocean --dns-digitalocean-credentials='"'$TOKEN_INI'"'
+        certbot certonly --non-interactive --agree-tos --email="$email" --domain="$domain" --dns-digitalocean --dns-digitalocean-credentials="$TOKEN_INI"
+        sleep .5
+        if certbot certificates 2>/dev/null | grep -q -o "Certificate Name: ${domain}";then
+            __; green Certificate obtained.; _.
+        else
+            __; red Certificate not found.; x
+        fi
+        ____
+    fi
+
+    if [[ "$dns_authenticator" == 'standalone' ]]; then
+        chapter Memaksa mematikan proses yang me-listen port 80.
+        code 'kill $(lsof -i :80 -t)'
+        while IFS= read -r line; do
+            if [ -n "$line" ];then
+                kill -9 $line
+            fi
+        done <<< `lsof -i :80 -t`
+        ____
+
+        chapter Request Certificate.
+        code certbot certonly --non-interactive --agree-tos --email="$email" --domain="$domain" --standalone
+        certbot certonly --non-interactive --agree-tos --email="$email" --domain="$domain" --standalone
         sleep .5
         if certbot certificates 2>/dev/null | grep -q -o "Certificate Name: ${domain}";then
             __; green Certificate obtained.; _.
@@ -326,7 +357,7 @@ exit 0
 # --no-hash-bang \
 # --no-original-arguments \
 # --no-error-invalid-options \
-# --no-error-require-arguments << EOF | clip
+# --no-error-require-arguments << EOF
 # FLAG=(
 # --fast
 # --version
@@ -344,6 +375,6 @@ exit 0
 # )
 # CSV=(
     # long:--digitalocean,parameter:dns_authenticator,type:flag,flag_option:true=digitalocean
+    # long:--standalone,parameter:dns_authenticator,type:flag,flag_option:true=standalone
 # )
 # EOF
-# clear
