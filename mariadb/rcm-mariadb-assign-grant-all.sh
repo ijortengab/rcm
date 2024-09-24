42750645937f91b92d04a1799f553db69f67cdc4
ijortengab <m_roji28@yahoo.com>
2024-09-24 08:19:44 +0700

    Block Dependency sekarang sudah bisa terdapat version dengan delimiter titik dua.

--- mariadb/rcm-mariadb-assign-grant-all.sh
+++ mariadb/rcm-mariadb-assign-grant-all.sh
@@ -125,7 +125,7 @@ EOF
 
 # Dependency.
 while IFS= read -r line; do
-    [[ -z "$line" ]] || command -v "${line}" >/dev/null || { echo -e "\e[91m""Unable to proceed, "'`'"${line}"'`'" command not found." "\e[39m"; exit 1; }
+    [[ -z "$line" ]] || command -v `cut -d: -f1 <<< "${line}"` >/dev/null || { echo -e "\e[91m""Unable to proceed, "'`'"${line}"'`'" command not found." "\e[39m"; exit 1; }
 done <<< `printHelp 2>/dev/null | sed -n '/^Dependency:/,$p' | sed -n '2,/^\s*$/p' | sed 's/^ *//g'`
 
 # Title.
