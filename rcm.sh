16d6cde8949d8d891b04e0b63d550336ccb46b29
ijortengab <m_roji28@yahoo.com>
2024-11-18 11:07:46 +0700

    Update function sleepExtended, menambah INDENT.

--- rcm.sh
+++ rcm.sh
@@ -1560,7 +1560,7 @@ Rcm_github_release() {
     esac
 }
 sleepExtended() {
-    local countdown=$1 sleep
+    local countdown=$1
     local width=$2
     if [ -z "$width" ];then
         width=80
@@ -1570,14 +1570,14 @@ sleepExtended() {
         _dikali10=$dikali10
         _dotLength=$(( ( width * _dikali10 ) / dikali10 ))
         printf "\r\033[K" >&2
-        printf %"$_dotLength"s | tr " " "." >&2
+        e; printf %"$_dotLength"s | tr " " "." >&2
         printf "\r"
         while [ "$_dikali10" -ge 0 ]; do
             dotLength=$(( ( width * _dikali10 ) / dikali10 ))
             if [[ ! "$dotLength" == "$_dotLength" ]];then
                 _dotLength="$dotLength"
                 printf "\r\033[K" >&2
-                printf %"$dotLength"s | tr " " "." >&2
+                e; printf %"$dotLength"s | tr " " "." >&2
                 printf "\r"
             fi
             _dikali10=$((_dikali10 - 1))
