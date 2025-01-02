1c3dfcbd7bec3c1ec978192bec4cbfc32c2bf708
IjorTengab <m_roji28@yahoo.com>
2025-01-02 11:20:59 +0700

    Draft rcm-add-ram-4g.sh release to rcm-system-ram-swap-4g.sh.

--- .draft/rcm-add-ram-4g.sh
+++
@@ -1,4 +0,0 @@
-fallocate -l 4G /swapfile2
-chmod 600 /swapfile2
-mkswap /swapfile2
-swapon /swapfile2
---
+++ system/rcm-system-ram-swap-4g.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Common Functions.
+red() { echo -ne "\e[91m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
+green() { echo -ne "\e[92m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
+yellow() { echo -ne "\e[93m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
+blue() { echo -ne "\e[94m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
+magenta() { echo -ne "\e[95m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
+error() { echo -n "$INDENT" >&2; red '#' "$@" >&2; echo >&2; }
+success() { echo -n "$INDENT" >&2; green '#' "$@" >&2; echo >&2; }
+chapter() { echo -n "$INDENT" >&2; yellow '#' "$@" >&2; echo >&2; }
+title() { echo -n "$INDENT" >&2; blue '#' "$@" >&2; echo >&2; }
+code() { echo -n "$INDENT" >&2; magenta "$@" >&2; echo >&2; }
+x() { echo >&2; exit 1; }
+e() { echo -n "$INDENT" >&2; echo -n "$@" >&2; }
+_() { echo -n "$INDENT" >&2; echo -n "#" "$@" >&2; }
+_,() { echo -n "$@" >&2; }
+_.() { echo >&2; }
+__() { echo -n "$INDENT" >&2; echo -n "#" '    ' >&2; [ -n "$1" ] && echo "$@" >&2 || echo -n  >&2; }
+____() { echo >&2; [ -n "$delay" ] && sleep "$delay"; }
+
+# Functions. Help and Version.
+printVersion() {
+    echo '0.16.12'
+}
+printHelp() {
+    title RCM System RAM Swap
+    _ 'Variation '; yellow 4G; _.
+    _ 'Version '; yellow `printVersion`; _.
+    _.
+cat << EOF
+Usage: rcm-system-ram-swap-4g [options]
+
+Global Options:
+   --fast
+        No delay every subtask.
+   --version
+        Print version of this script.
+   --help
+        Show this help.
+   --root-sure
+        Bypass root checking.
+EOF
+}
+
+# Help and Version.
+[ -n "$help" ] && { printHelp; exit 1; }
+[ -n "$version" ] && { printVersion; exit 1; }
+
+code fallocate -l 4G /swapfile-rcm-4G
+code chmod 600 /swapfile-rcm-4G
+code mkswap /swapfile-rcm-4G
+code swapon /swapfile-rcm-4G
+
+if [ ! -f /swapfile-rcm-4G ];then
+    fallocate -l 4G /swapfile-rcm-4G
+    chmod 600 /swapfile-rcm-4G
+    mkswap /swapfile-rcm-4G
+    swapon /swapfile-rcm-4G
+fi
+
+exit 0
