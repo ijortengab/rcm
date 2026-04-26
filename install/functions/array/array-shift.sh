25947740084fbbfc7f84ff512596ff47a20a9739
ijortengab <m_roji28@yahoo.com>
2026-04-26 13:36:45 +0700

    [array-shift.sh] Rename function from ArrayShift to array-shift. BREAK COMPATIBILITY, the output of function is different.

--- install/functions/array/array-shift.sh
+++ install/functions/array/array-shift.sh
@@ -1,7 +1,8 @@
 # Shift an element off the beginning of array.
 #
 # Globals:
-#   Modified: _return
+#   Modified: _return_value
+#             _return_array
 #
 # Arguments:
 #   1 = Parameter of the array.
@@ -12,15 +13,22 @@
 # Example:
 #   ```
 #   my=("cherry" "manggo" "blackberry" "manggo" "blackberry")
-#   ArrayShift my[@]
-#   # Get result in variable `$_return`.
-#   # _return=("manggo" "blackberry" "manggo" "blackberry")
+#   array-shift my[@]
+#   # Get result in variable `$_return_value and $_return_array`.
+#   # _return_value=cherry
+#   # _return_array=("manggo" "blackberry" "manggo" "blackberry")
 #   ```
-ArrayShift() {
+array-shift() {
+    # global _return_value
+    # global _return_array
     local index
     local source=("${!1}")
-    _return=()
+    unset _return_value
+    unset _return_array
+    _return_value=
+    _return_array=()
     for (( index=1; index < ${#source[@]} ; index++ )); do
-        _return+=("${source[$index]}")
+        _return_array+=("${source[$index]}")
     done
+    _return_value="${source[0]}"
 }
