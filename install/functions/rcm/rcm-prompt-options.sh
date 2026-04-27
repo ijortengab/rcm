aaffadba6e3538415e8e448f02f608eb5e9fa767
ijortengab <m_roji28@yahoo.com>
2026-04-27 10:59:26 +0700

    [rcm-prompt-options.sh] Variable RCM_LIB menjadi standard untuk include source script.

--- install/functions/rcm/rcm-prompt-options.sh
+++ install/functions/rcm/rcm-prompt-options.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /usr/local/rcm/$RCM_VERSION/functions/rcm/rcm-prompt-options-option.sh
+source "$RCM_LIB"/functions/rcm/rcm-prompt-options-option.sh
 
 rcm-prompt-options() {
     # Required Global variable.
