8bcc5b85692389bc39d72e0bc1536506f3efba27
ijortengab <m_roji28@yahoo.com>
2026-05-04 12:51:00 +0700

    [rcm-prompt.sh] Memulai parsing label Additional Options pada help. Method build-command di perbaiki. Memulai method parse-to-include. Variable RCM_YAML yang mana hasil generate dari rcm-prompt-options dan juga parsing 'Additional Options', dimasukkan ke variable RCM_PROMPT_YAML.

--- install/functions/rcm/rcm-prompt.sh
+++ install/functions/rcm/rcm-prompt.sh
@@ -13,28 +13,31 @@ rcm-prompt() {
     local value
     local command="$1"
     local label list_to_execute
+    local indent
+    local default_indent='  '
+    local each
 
     build-command() {
-        local _RCM_PROMPT_CHAIN
-        echo-wrap 'Use command below to return to the last dialog.' 0
-        local shortoptions
-        [ -n "$interactive" ] && shortoptions+='i'
-        [ -n "$autoyes" ] && shortoptions+='y'
-        [ -n "$timer" ] && shortoptions+='t'
-        [ -z "$fast" ] && shortoptions+='s'
+        local each
+        local rcm_options=
+        local rcm_options_array=()
+        local words_array=()
+        [ -n "$interactive" ] && rcm_options_array+=(i)
+        [ -n "$autoyes" ] && rcm_options_array+=(y)
+        [ -n "$timer" ] && rcm_options_array+=(t)
+        [ -z "$fast" ] && rcm_options_array+=(s)
         if [ -n "$verbose" ];then
             for ((i = 0 ; i < "$verbose" ; i++)); do
-                shortoptions+='v'
+                rcm_options_array+=(v)
             done
         fi
-        [ -n "$shortoptions" ] && shortoptions=" -${shortoptions}"
-        if [ -z "$RCM_PROMPT_CHAIN" ];then
-            _RCM_PROMPT_CHAIN="rcm${shortoptions} ${extension}"
-        else
-            _RCM_PROMPT_CHAIN="$RCM_PROMPT_CHAIN"
-        fi
-        for each in "${RCM_ARGUMENT_PREVIEW[@]}"; do _RCM_PROMPT_CHAIN+=" ${each}"; done
-        words_array=($_RCM_PROMPT_CHAIN)
+        for each in "${rcm_options_array[@]}";do
+            rcm_options="$each"
+        done
+        [ -n "$rcm_options" ] && rcm_options="-${rcm_options}"
+        words_array+=(rcm $rcm_options)
+        words_array+=("${RCM_EXTENSION_CHAIN[@]}")
+        words_array+=("${RCM_ARGUMENT_PREVIEW[@]}")
         echo-wrap-multiline
     }
 
@@ -47,6 +50,57 @@ rcm-prompt() {
         exit 0
     }
 
+    # Mem-parse chapter $label pada contents, yang digunakan untuk list
+    # execute command.
+    parse-to-include() {
+        local label="$1"
+        local contents="$2"
+        local first_line_trimmed is_valid
+        local command arguments
+        local line find replace
+        [ -z "$label" ] && { error "Argument <label> is required."; x; }
+        [ -z "$contents" ] && { error "Argument <contents> is required."; x; }
+
+        build-command
+        _.;
+
+        chapter "$label" include.
+        ____
+
+        until [[ -z "$contents" ]];do
+            first_line_trimmed=`sed -n 1p <<< "$contents" | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//'`
+            contents=`sed -n '2,$p' <<< "$contents"`
+            is_valid=$(echo "$first_line_trimmed" | sed -n -E 's/\s*([^\)]+\))/\1/p')
+            if [ -z "$is_valid" ];then
+                error The command format is not valid: '`'"$first_line_trimmed"'`'.; x;
+            fi
+            command=$(echo "$first_line_trimmed" | sed -n -E 's/^([^\(]+)\(([^\)]*)\)$/\1/p')
+            arguments=$(echo "$first_line_trimmed" | sed -n -E 's/^([^\(]+)\(([^\)]*)\)$/\2/p')
+
+            if ! command -v "$command" > /dev/null;then
+                error The command is not found.; x;
+            fi
+            if [ -n "$RCM_ARGUMENT_PLACEHOLDERS" ];then
+                while read line; do
+                    find=$(echo ${line} | sed -E 's|^([^:]+):.*|\1|' | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//')
+                    replace=$(echo ${line} | sed -E 's|^[^:]+:(.*)|\1|' | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//')
+                    # description="${description/"$find"/"$replace"}"
+                    if [ -n "$arguments" ];then
+                        arguments="${arguments/"$find"/"$replace"}"
+                    fi
+                done <<< "$RCM_ARGUMENT_PLACEHOLDERS"
+            fi
+            [ -n "$arguments" ] && arguments=' '"$arguments"
+            # chapter "$label" command.
+
+            INDENT+="$RCM_INDENT"; export INDENT="$INDENT"
+            source `${command}${arguments}` \
+                ; [ ! $? -eq 0 ] && x
+            INDENT="${INDENT::-${#RCM_INDENT}}"
+
+        done
+    }
+
     trap trap-sigint SIGINT
 
     # Populate options.
@@ -56,4 +110,20 @@ rcm-prompt() {
         rcm-prompt-options
     fi
 
+    label='Additional Options'
+    list_to_include=`echo "$contents" | sed -n '/^'"$label"'[:\.]$/,$p' | sed -n '1,/^\s*$/p' | sed -n '2,/^\s*$/p'`
+    if [ -n "$list_to_include" ];then
+        parse-to-include "$label" "$list_to_include"
+    fi
+
+    indent=''
+    for each in "${RCM_EXTENSION_CHAIN[@]}"; do
+        RCM_PROMPT_YAML+="${indent}${each}:"$'\n'
+        indent+="$default_indent"
+    done
+    while IFS= read -r line; do
+        if [ -n "$line" ];then
+            RCM_PROMPT_YAML+="${indent}${line}"$'\n'
+        fi
+    done <<< "$RCM_YAML"
 }
