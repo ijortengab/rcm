668b3f008530edf4559180c74cfdaa261dddb4c3
ijortengab <m_roji28@yahoo.com>
2026-05-15 19:30:54 +0700

    [array.sh] Menambah method do-unset() pada object array.

--- install/functions/array/array.sh
+++ install/functions/array/array.sh
@@ -461,6 +461,122 @@ array() {
         [ -n "${part_3}" ] && array+="${part_3}"
         [ -n "${part_4}" ] && array+="${part_4}"
     }
+
+    do-unset() {
+        local array_local="$array"
+        local args=()
+        local count find found below each
+        local value
+        local line_1
+        local line_2
+        local line_3
+        local part_1
+        local part_2
+        local part_3
+        local part_4
+        local indent
+        local parent
+
+        while [ $# -gt 0 ]; do
+            args+=("$1"); shift
+        done
+
+        until [ ${#args[@]} -eq 0 ];do
+            array-shift args[@]; args=("${_return_array[@]}")
+            each="$_return_value"
+
+            find='^'"${each}:"
+            found=$(grep -n -- "$find" <<< "$array_local" | tail -1)
+            if [ -z "$found" ];then
+                break
+            fi
+
+            line_number_found=$(cut -d: -f1 <<< "$found")
+            line_string_found=$(cut -d: -f2- <<< "$found")
+            find='^'"${each}:\s+(.*)"
+            value=$(grep -E -- "${find}" <<< "$line_string_found" | sed -E 's|'"${find}"'|\1|')
+
+            if [ -z "$line_1" ];then
+                line_1=$line_number_found
+            else
+                line_1=$(( line_1 + line_number_found))
+            fi
+            line_2=$line_1
+
+            array_local_child=
+            unset count; declare -i count; count=$line_number_found
+            while true; do
+                count+=1
+                below=`sed -n ${count}p <<< "$array_local"`
+                find='^'"${default_indent}"
+                if grep -q -- "$find" <<< "$below";then
+                    below_ltrim=$(grep -E -- "${find}" <<< "$below" | sed -E 's|'"${find}(.+)"'|\1|')
+                    array_local_child+="$below_ltrim"$'\n'
+                    line_2=$(( line_2 + 1))
+                else
+                    break
+                fi
+            done
+
+            # Tambah indent hanya jika belum key terakhir.
+            if [ ${#args[@]} -gt 0 ];then
+                indent+="${default_indent}"
+            fi
+
+            array_local="$array_local_child"
+
+            # Save parent key.
+            parent="$each"
+        done
+
+        if [ -n "$line_1" ];then
+            if [[ "$line_1" -eq 1 ]];then
+                part_1=
+            else
+                part_1=$(sed -n '1,'$((line_1 - 1))'p' <<< "$array")
+            fi
+
+            if [[ "$line_2" -gt "$line_1" ]];then
+                part_2=$(sed -n $line_1','$line_2'p' <<< "$array")
+                # Debug jika diperlukan, lalu clear.
+                part_2=
+                line_3=$((line_2+1))
+            else
+                part_2=
+                line_3=$((line_1+1))
+            fi
+            part_3=$(sed -n $line_3',$p' <<< "$array")
+        fi
+
+        if [ -n "$part_1" ];then
+            if [[ ! "${part_1:(-1)}" == $'\n' ]];then
+                part_1+=$'\n'
+            fi
+        fi
+        if [ -n "$part_2" ];then
+            if [[ ! "${part_2:(-1)}" == $'\n' ]];then
+                part_2+=$'\n'
+            fi
+        fi
+        if [ -n "$part_3" ];then
+            if [[ ! "${part_3:(-1)}" == $'\n' ]];then
+                part_3+=$'\n'
+            fi
+        fi
+        if [ -n "$part_4" ];then
+            if [[ ! "${part_4:(-1)}" == $'\n' ]];then
+                part_4+=$'\n'
+            fi
+        fi
+
+        # Reset.
+        array=
+        [ -n "${part_1}" ] && array+="${part_1}"
+        [ -n "${part_2}" ] && array+="${part_2}"
+        [ -n "${part_3}" ] && array+="${part_3}"
+        [ -n "${part_4}" ] && array+="${part_4}"
+    }
+
     if [ ${#args[@]} -gt 2 ];then
         array-pop args[@]; args=("${_return_array[@]}")
         last_one="$_return_value"
