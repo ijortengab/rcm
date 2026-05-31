642565c6fb3dce628cf6237c2d0afa68ceadaf73
ijortengab <m_roji28@yahoo.com>
2026-06-01 04:39:59 +0700

    [require.sh] Menambah function require() dan include().

--- install/require.sh
+++ install/require.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 red() { echo -ne "\e[91m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
 green() { echo -ne "\e[92m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
 yellow() { echo -ne "\e[93m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
@@ -17,3 +19,48 @@ __() { echo -n "$INDENT" >&2; echo -n "# ${RCM_INDENT}" >&2; [ -n "$1" ] && echo
 ___() { echo -n "$INDENT" >&2; echo -n "# ${RCM_INDENT}${RCM_INDENT}" >&2; [ -n "$1" ] && echo "$@" >&2 || echo -n  >&2; }
 ____() { echo >&2; [ -n "$RCM_DELAY" ] && sleep "$RCM_DELAY"; }
 ----() { echo -n "$RCM_INDENT"; }
+
+require() {
+    if [ "$1" == command ];then
+        command -v "$2" >/dev/null || { error Unable to proceed, command not found: '`'"$2"'`'.; x; }
+        return 0
+    fi
+    if [ "$1" == rcm ];then
+        local prefix="$RCM_LIB"
+        local command_file_sh="rcm"
+        shift
+        while [[ $# -gt 0 ]]; do
+            prefix+='/commands/'$1
+            command_file_sh+="-${1}"
+            shift
+        done
+        command_file_sh+='.sh'
+        OLDPATH="$PATH"
+        PATH="$prefix":"$PATH"
+        command -v "$command_file_sh" >/dev/null || { red "Unable to proceed, $command_file_sh command not found."; x; }
+        PATH="$OLDPATH"
+        return 0
+    fi
+    local filename="$1"
+    if [[ ! "${filename:0:1}" == / ]];then
+        filename="${RCM_LIB}/${filename}"
+    fi
+    local basename=$(basename "$filename")
+    local dirname=$(dirname "$filename")
+    if [ ! -f "$filename" ];then
+        e Require:' '; magenta "${filename}"; _, '.'; _.
+        red 'Process terminated.'; _, ' File is not found: '; yellow "$basename"; _, .; x
+    fi
+    . "$filename"
+}
+
+include() {
+    local filename="$1"
+    INDENT+="$RCM_INDENT"; export INDENT="$INDENT"
+    if [ ! -f "$filename" ];then
+        e Require:' '; magenta "${filename}"; _, '.'; _.
+        red 'Process terminated.'; _, ' File is not found: '; yellow "$basename"; _, .; x
+    fi
+    . "$filename"
+    INDENT="${INDENT::-${#RCM_INDENT}}"
+}
