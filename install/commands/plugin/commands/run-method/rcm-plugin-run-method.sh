df109049990b2bbda4f39567a8371d9a3a1c4ac0
ijortengab <m_roji28@yahoo.com>
2026-06-18 09:53:18 +0700

    Add new command: rcm plugin run-method. Add new file rcm-plugin-run-method.sh. The command 'rcm plugin run-method' is alias of 'rcm plugin get-socket'. Make clear that socket will act as a method.

---
+++ install/commands/plugin/commands/run-method/rcm-plugin-run-method.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+RCM_EXTENSION_VERSION=0.19.0-alpha.9
+
+# Usage Functions.
+usage() {
+    cat << EOF
+Usage: rcm plugin run-method
+
+Alias of rcm plugin get-socket.
+
+Make clear that socket will act as a method.
+EOF
+}
+
+# Prevent scripts from being executed directly.
+[ -f "${RCM_LIB}/require.sh" ] && source "${RCM_LIB}/require.sh" || { usage >&2; exit 1; }
+
+# ------------------------------------------------------------------------------
+
+# Title.
+title rcm plugin run-method "$@"
+____
+
+# Dependency.
+require rcm plugin get-socket
+
+INDENT+="    " \
+rcm plugin get-socket "$@" \
+    --hide-title \
+    ; [ ! $? -eq 0 ] && x
+
+exit 0
