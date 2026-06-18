7d062fd59f6adb3b7b4ab21a46d8a5ed6c97c30f
ijortengab <m_roji28@yahoo.com>
2026-06-18 09:54:19 +0700

    Add new command: rcm plugin use-trait. Add new file rcm-plugin-use-trait.sh. The command 'rcm plugin use-trait' is alias of 'rcm plugin get-socket'. Make clear that socket will act as a trait.

---
+++ install/commands/plugin/commands/use-trait/rcm-plugin-use-trait.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+RCM_EXTENSION_VERSION=0.19.0-alpha.9
+
+# Usage Functions.
+usage() {
+    cat << EOF
+Usage: rcm plugin use-trait
+
+Alias of rcm plugin get-socket.
+
+Make clear that socket will act as a trait.
+EOF
+}
+
+# Prevent scripts from being executed directly.
+[ -f "${RCM_LIB}/require.sh" ] && source "${RCM_LIB}/require.sh" || { usage >&2; exit 1; }
+
+
+# ------------------------------------------------------------------------------
+
+# Title.
+title rcm plugin use-trait "$@"
+____
+
+# Dependency.
+require rcm plugin get-socket
+
+INDENT+="    " \
+rcm plugin get-socket "$@" \
+    --hide-title \
+    ; [ ! $? -eq 0 ] && x
+
+exit 0
