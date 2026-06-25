8ef06d9683b95185f2469c80bd893a61fb6ad300
ijortengab <m_roji28@yahoo.com>
2026-06-26 05:43:20 +0700

    Add new command: rcm plugin prompt. Add new file rcm-plugin-prompt.sh. The command 'rcm plugin prompt' is alias of 'rcm plugin get-socket'. Make clear that socket will do the prompt.

---
+++ install/commands/plugin/commands/prompt/rcm-plugin-prompt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+RCM_EXTENSION_VERSION=0.19.0-alpha.10
+
+# Usage Functions.
+usage() {
+    cat << EOF
+Usage: rcm plugin prompt
+
+Alias of rcm plugin get-socket.
+
+Make clear that socket will do the prompt.
+EOF
+}
+
+# Prevent scripts from being executed directly.
+[ -f "${RCM_LIB}/require.sh" ] && source "${RCM_LIB}/require.sh" || { usage >&2; exit 1; }
+
+require rcm plugin get-socket
+
+INDENT+="    " \
+rcm plugin get-socket "$@" \
+    --hide-title \
+    ; [ ! $? -eq 0 ] && x
+
+exit 0
