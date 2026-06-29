70904fbfcc055bccb77bbd4075e02d94a33c6322
ijortengab <m_roji28@yahoo.com>
2026-06-29 09:38:34 +0700

    Set version 0.19.0-alpha.11.

--- install/commands/postfix/commands/init/rcm-postfix-init.sh
+++ install/commands/postfix/commands/init/rcm-postfix-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RCM_EXTENSION_VERSION=0.19.0-alpha.10
+RCM_EXTENSION_VERSION=0.19.0-alpha.11
 
 # Usage Functions.
 usage() {
--- rcm.sh
+++ rcm.sh
@@ -10,7 +10,7 @@
 #
 
 RCM_PREFIX=${RCM_PREFIX:=/usr/local/rcm}
-RCM_VERSION='0.19.0-alpha.10'
+RCM_VERSION='0.19.0-alpha.11'
 RCM_LIB="${RCM_PREFIX}/lib/${RCM_VERSION}"
 GITHUB_OWNER=ijortengab
 GITHUB_REPOSITORY=rcm
