4242b8496a21bdffd1fb260a2b9faa9ee3520ea5
ijortengab <m_roji28@yahoo.com>
2026-07-23 10:19:44 +0700

    [rcm-cygwin-setup-wsl-autorun-crond.sh] Penyesuaian nama baru: rcm cygwin setup wsl autorun crond.

--- install/commands/cygwin/commands/setup/commands/wsl/commands/autorun/commands/crond/rcm-cygwin-setup-wsl-autorun-crond.sh
+++ install/commands/cygwin/commands/setup/commands/wsl/commands/autorun/commands/crond/rcm-cygwin-setup-wsl-autorun-crond.sh
@@ -5,7 +5,7 @@ RCM_EXTENSION_VERSION=0.19.0-alpha.13
 # Usage Functions.
 usage() {
     cat << EOF
-Usage: rcm-cron-setup-wsl-autorun-crond
+Usage: rcm cygwin setup wsl autorun crond
 
 Global Options:
    --version
@@ -47,7 +47,7 @@ BASENAME=${BASENAME:=host-trigger-wsl-autorun-crond}
 # ------------------------------------------------------------------------------
 
 # Title.
-title rcm-cron-setup-wsl-autorun-crond
+title rcm cygwin setup wsl autorun crond
 ____
 
 # Dependency.
