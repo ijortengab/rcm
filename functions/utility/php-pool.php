bc6a35ef55f0aeb47b8c1e1514e124d67a54b6b3
ijortengab <m_roji28@yahoo.com>
2026-08-14 04:19:05 +0700

    [php-pool.php] Menambah function _die(). Menambah mode get-info.

--- functions/utility/php-pool.php
+++ functions/utility/php-pool.php
@@ -1,5 +1,12 @@
 <?php
 
+// The builtin function die() is not write error to stderr, we create
+// alternative function named _die().
+function _die($string='', $code = 1) {
+    fwrite(STDERR, $string.PHP_EOL);
+    exit($code);
+}
+
 $mode = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1]: null;
 
 switch ($mode) {
@@ -20,5 +27,30 @@ switch ($mode) {
         foreach ($keys as $key) {
             echo $key.PHP_EOL;
         }
+        break;
+        ;;
+
+    case 'get-info':
+        $pool_name = isset($_SERVER['argv'][2]) ? $_SERVER['argv'][2]: null;
+        if (!isset($pool_name)) {
+            _die('Argument <pool_name> is required.');
+        }
+        $key = isset($_SERVER['argv'][3]) ? $_SERVER['argv'][3]: null;
+        if (!isset($key)) {
+            _die('Argument <key> is required.');
+        }
+        $stdin = '';
+        while (FALSE !== ($line = fgets(STDIN))) {
+           $stdin .= $line;
+        }
+        $array_master = parse_ini_string($stdin, true);
+        if (!array_key_exists($pool_name, $array_master)) {
+            _die('The pool is not exists: '.$pool_name);
+        }
+        if (!array_key_exists($key, $array_master[$pool_name])) {
+            _die('The key is not exists: '.$key);
+        }
+        echo $array_master[$pool_name][$key].PHP_EOL;
+        break;
         ;;
 }
