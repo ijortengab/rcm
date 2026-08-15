fc28968cc53dc5be1a98135c6d485645e05f2c8a
ijortengab <m_roji28@yahoo.com>
2026-08-15 22:12:56 +0700

    [rcm-php-get-info-pool.sh] Menghapus option --php-fpm-user. Require php-pool.sh.

--- install/commands/php/commands/get-info/commands/pool/rcm-php-get-info-pool.sh
+++ install/commands/php/commands/get-info/commands/pool/rcm-php-get-info-pool.sh
@@ -10,8 +10,6 @@ Usage: rcm php get-info pool [options]
 Options:
    --php-version=VERSION
         Set version of PHP.
-   --php-fpm-user=USER
-        Set the Unix user that used by PHP FPM.
    --pool-name=NAME
         Set the pool name.
    --key=KEY
@@ -37,8 +35,6 @@ while [[ $# -gt 0 ]]; do
         --version) version=1; shift ;;
         --key=*) key="${1#*=}"; shift ;;
         --key) if [[ ! $2 == "" && ! $2 =~ (^--$|^-[^-]|^--[^-]) ]]; then key="$2"; shift; fi; shift ;;
-        --php-fpm-user=*) php_fpm_user="${1#*=}"; shift ;;
-        --php-fpm-user) if [[ ! $2 == "" && ! $2 =~ (^--$|^-[^-]|^--[^-]) ]]; then php_fpm_user="$2"; shift; fi; shift ;;
         --php-version=*) php_version="${1#*=}"; shift ;;
         --php-version) if [[ ! $2 == "" && ! $2 =~ (^--$|^-[^-]|^--[^-]) ]]; then php_version="$2"; shift; fi; shift ;;
         --pool-name=*) pool_name="${1#*=}"; shift ;;
@@ -50,38 +46,42 @@ done
 set -- "${_new_arguments[@]}"
 unset _new_arguments
 
+# Define variables and constants.
+PHP_FPM_POOL_DIRECTORY=${PHP_FPM_POOL_DIRECTORY:=/etc/php/[php-version]/fpm/pool.d}
+
 # Help and Version.
 [ -n "$help" ] && { usage; exit 0; }
 [ -n "$version" ] && { e $RCM_EXTENSION_VERSION; x; }
 
+# Require.
+require vendor/ijortengab/rcm/functions/utility/php-pool.sh
+
 # ------------------------------------------------------------------------------
 
-# Define variables and constants.
-PHP_FPM_POOL_DIRECTORY=${PHP_FPM_POOL_DIRECTORY:=/etc/php/[php-version]/fpm/pool.d}
+# Title.
+title rcm php get-info pool
+____
 
-php=$(cat <<'EOF'
-$mode = $_SERVER['argv'][1];
-switch ($mode) {
-    case 'get':
-        $file = $_SERVER['argv'][2];
-        $section_name = $_SERVER['argv'][3];
-        $what = $_SERVER['argv'][4];
-        if (file_exists($file)) {
-            $array = parse_ini_file($file, true);
-            if (array_key_exists($section_name, $array)) {
-                if (array_key_exists($what, $array[$section_name])) {
-                    echo $array[$section_name][$what];
-                    exit(0);
-                }
-            }
-        }
-        exit(1);
-        break;
-}
-EOF
-)
+# Dependency.
+require command php
+
+chapter Mapping operand as value of options.
+if [ -n "$1" ];then
+    code --php-version=$1
+    php_version="$1"; shift
+fi
+if [ -n "$1" ];then
+    code --pool-name=$1
+    pool_name=$1; shift
+fi
+if [ -n "$1" ];then
+    code --key=$1
+    key=$1; shift
+fi
+____
 
 # Requirement, validate, and populate value.
+chapter Variable dump.
 if [ -z "$php_version" ];then
     error "Argument --php-version required."; x
 fi
@@ -94,32 +94,28 @@ if [ -z "$key" ];then
     error "Argument --key required."; x
 fi
 code 'key="'$key'"'
-# Rename variable.
-section_name="$pool_name"
-code 'section_name="'$section_name'"'
-find='[php-version]'
-replace="$php_version"
-PHP_FPM_POOL_DIRECTORY="${PHP_FPM_POOL_DIRECTORY/"$find"/"$replace"}"
+____
 
-found=
-found_file=
-while read file; do
-    if grep -q -F "[$section_name]" <<< `sed '/^;/d' "$file"`;then
-        found=1
-        found_file="$file"
-        break;
+if [ "$EUID" -ne 0 ];then
+    find='[php-version]'
+    replace="$php_version"
+    PHP_FPM_POOL_DIRECTORY="${PHP_FPM_POOL_DIRECTORY/"$find"/"$replace"}"
+    if [ ! -d "$PHP_FPM_POOL_DIRECTORY" ];then
+        error PHP Version is not exists in system.; x
     fi
-done <<< `ls "$PHP_FPM_POOL_DIRECTORY"/*.conf`
-
-if [ -z "$found_file" ];then
-    error File config that contains section is not found.; x
-elif [ -n "$php_fpm_user" ];then
-    user=$(php -r "$php" get "$found_file" "$section_name" "user")
-    if [ ! "$php_fpm_user" == "$user" ];then
-        error File config that contains section is not belong to user '`'"$php_fpm_user"'`'.; x
-    fi
+    contents=
+    while read file; do
+        contents+=$(cat - < "$file")
+        contents+=$'\n'
+    done <<< `ls "$PHP_FPM_POOL_DIRECTORY"/*.conf`
+    echo "$contents" | php-pool get-info $pool_name $key \
+    ; [ ! $? -eq 0 ] && x
+else
+    # Dependency.
+    require command php-fpm$php_version
+    php-fpm$php_version -tt 2>&1 | sed -E 's/.*NOTICE:[[:blank:]]+//' | sed '$d' | php-pool get-info $pool_name $key \
+    ; [ ! $? -eq 0 ] && x
 fi
-php -r "$php" get "$found_file" "$section_name" "$key"
 
 # parse-options.sh \
 # --without-end-options-double-dash \
@@ -135,7 +131,6 @@ php -r "$php" get "$found_file" "$section_name" "$key"
 # )
 # VALUE=(
 # --php-version
-# --php-fpm-user
 # --pool-name
 # --key
 # )
