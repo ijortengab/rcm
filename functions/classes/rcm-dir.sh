e7cf87d08cc662cf93def81bc67722b927113e08
ijortengab <m_roji28@yahoo.com>
2026-09-10 13:36:43 +0700

    [rcm-dir.sh] Optimasi dengan menghindari command basename dan dirname.

--- functions/classes/rcm-dir.sh
+++ functions/classes/rcm-dir.sh
@@ -14,23 +14,23 @@ rcm-dir() {
             found=
             notfound=
             if [ -d "$path" ];then
-                __ Direktori '`'$(basename "$path")'`' ditemukan.
+                __ Direktori '`'"${path##*/}"'`' ditemukan.
                 found=1
             else
-                __ Direktori '`'$(basename "$path")'`' tidak ditemukan.
+                __ Direktori '`'"${path##*/}"'`' tidak ditemukan.
                 notfound=1
             fi
             ;;
         mustExists)
             if [ -d "$path" ];then
-                __; green Direktori '`'$(basename "$path")'`' ditemukan.; _.
+                __; green Direktori '`'"${path##*/}"'`' ditemukan.; _.
             else
-                __; red Direktori '`'$(basename "$path")'`' tidak ditemukan.; x
+                __; red Direktori '`'"${path##*/}"'`' tidak ditemukan.; x
             fi
             ;;
         terminateIfNotExists)
             if [ ! -d "$path" ];then
-                __; red Direktori '`'$(basename "$path")'`' tidak ditemukan.; x
+                __; red Direktori '`'"${path##*/}"'`' tidak ditemukan.; x
             fi
             ;;
     esac
@@ -43,26 +43,28 @@ rcm-dir() {
 # `dirMustExists "$dir"` digantikan dengan `rcm-dir "$dir" mustExists`
 # ``
 # dirMustExists() {
+    # local path="$1"
     # global used:
     # global modified:
     # function used: __, success, error, x
-    # if [ -d "$1" ];then
-        # __; green Direktori '`'$(basename "$1")'`' ditemukan.; _.
+    # if [ -d "$path" ];then
+        # __; green Direktori '`'"${path##*/}"'`' ditemukan.; _.
     # else
-        # __; red Direktori '`'$(basename "$1")'`' tidak ditemukan.; x
+        # __; red Direktori '`'"${path##*/}"'`' tidak ditemukan.; x
     # fi
 # }
 # isDirExists() {
+    # local path="$1"
     # global used:
     # global modified: found, notfound
     # function used: __
     # found=
     # notfound=
-    # if [ -d "$1" ];then
-        # __ Direktori '`'$(basename "$1")'`' ditemukan.
+    # if [ -d "$path" ];then
+        # __ Direktori '`'"${path##*/}"'`' ditemukan.
         # found=1
     # else
-        # __ Direktori '`'$(basename "$1")'`' tidak ditemukan.
+        # __ Direktori '`'"${path##*/}"'`' tidak ditemukan.
         # notfound=1
     # fi
 # }
