f1ea37fa995b987f2f25ef33056962bab4eb180b
ijortengab <m_roji28@yahoo.com>
2026-09-24 11:48:21 +0700

    [require.sh] Add color function purple(). Modify function code(), enable to spesific case of code, set value and dump array.

--- install/require.sh
+++ install/require.sh
@@ -5,11 +5,16 @@ green() { echo -ne "\e[92m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
 yellow() { echo -ne "\e[93m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
 blue() { echo -ne "\e[94m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
 magenta() { echo -ne "\e[95m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
+purple() { echo -ne "\e[38;5;177m" >&2; echo -n "$@" >&2; echo -ne "\e[39m" >&2; }
 error() { echo -n "$INDENT" >&2; red '#' "$@" >&2; echo >&2; }
 success() { echo -n "$INDENT" >&2; green '#' "$@" >&2; echo >&2; }
 chapter() { echo -n "$INDENT" >&2; yellow '#' "$@" >&2; echo >&2; }
 title() { echo -n "$INDENT" >&2; blue '#' "$@" >&2; echo >&2; }
-code() { echo -n "$INDENT" >&2; magenta "$@" >&2; echo >&2; }
+code() {
+    code-set() { [ "${1#*=}" == @ ] && { code-set-array "${1%%=*}"; } || { magenta "${1%%=*}"; _, =; yellow \"; purple "${1#*=}"; yellow \"; } }
+    code-set-array() { local a="${1}[@]"; magenta "$1"; _, '=( '; _.;for i in "${!a}"; do echo -n "${INDENT}${RCM_INDENT}" >&2; yellow \"; purple "$i"; yellow \"; _.; done; e ')'; }
+    echo -n "$INDENT" >&2; [[ $# -eq 1 && "$1" =~ ^[0-9a-zA-Z_]+= ]] && { code-set "$1"; } || magenta "$@" >&2; echo >&2;
+}
 x() { echo "$@" >&2; exit 1; }
 e() { echo -n "$INDENT" >&2; echo -n "$@" >&2; }
 _() { echo -n "$INDENT" >&2; echo -n "#"' ' >&2; [ -n "$1" ] && echo -n "$@" >&2; }
